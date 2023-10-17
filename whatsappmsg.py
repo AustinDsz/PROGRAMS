2db8e853a23ea28d702da72d88d9f458be3cc6a7
unknown <austindsz21@gmail.com>
2023-10-17 21:39:14 +0530

    send a whatsapp message using python program

---
+++ whatsappmsg.py
@@ -0,0 +1,8 @@
+import pywhatkit
+
+
+pywhatkit.sendwhatmsg("+916361767544", 
+					"this is me from the past, im an a.i trained to replace your friend, your friend was kidnapped by c.i.a, he wont be back, this is his a a.i of his persona who chose to semd this message, this is a trick test, thank you", 
+					18, 30, 15, True, 3)
+
+
